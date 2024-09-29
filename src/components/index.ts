990d6346f0e20b71dd4def7e9d5be8bbe1b0b10a
hovani <oganisyanararat1@gmail.com>
2024-09-29 16:27:50 +0400

    home page done

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,17 @@ import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
 
-export {UpcomingEvents, DealsChart, UpcomingEventsSkeleton, AccordionHeaderSkeleton, ProjectCardSkeleton, KanbanColumnSkeleton, LatestActivitiesSkeleton}
+export {
+    UpcomingEvents,
+    DealsChart,
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    ProjectCardSkeleton,
+    KanbanColumnSkeleton,
+    LatestActivitiesSkeleton,
+    DashboardTotalCountCard, 
+    LatestActivities
+}
